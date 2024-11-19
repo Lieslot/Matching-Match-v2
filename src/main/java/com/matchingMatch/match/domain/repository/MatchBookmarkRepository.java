8866313f31dbcd44377cf749ff1c7638fd7dd7b6
Lieslot <rlaxodud1999@gmail.com>
2024-11-19 21:23:49 +0900

    chore: 매치 북마크 관련 부분 제거

--- src/main/java/com/matchingMatch/match/domain/repository/MatchBookmarkRepository.java
+++
@@ -1,8 +0,0 @@
-package com.matchingMatch.match.domain.repository;
-
-import com.matchingMatch.team.domain.entity.MatchBookmarkEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface MatchBookmarkRepository extends JpaRepository<MatchBookmarkEntity, Long> {
-}
